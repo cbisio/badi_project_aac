0d5004d7c6871c3c7cc8557c12f5104349e79321
unknown <arnald.auli@gmail.com>
2020-01-23 18:11:27 +0100

    Added extra validations for ensure correct data type.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :room
-  validates :url, :order, presence: true
+  validates :url, presence: true, length: { minimum: 5, maximum: 160 }
+  validates :order, presence: true
+  validates_numericality_of :order, greater_than_or_equal_to: 1
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,8 +2,10 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :photos
   has_and_belongs_to_many :extras
-  validates :description, :latitude, :longitude,  presence: true
-  validates :name,  presence: true, length: { minimum: 3, maximum: 15 }
-  validates :address,  presence: true, length: { minimum: 10, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 15 }
+  validates :address, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 200 }
   validates :price, presence: true, :numericality => { :greater_than => 10, :less_than => 1000 }
+  validates :latitude, presence: true, :numericality => { :greater_than => -90, :less_than => 90 }
+  validates :longitude, presence: true, :numericality => { :greater_than => -180, :less_than => 180 }
 end
