aa7a8503e3f2272d5211ea94d256a4d0fcebce7d
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2020-01-26 16:45:57 +0100

    Error handling configuration
    DONE:
     - exception handler module created inside 'V1/exception_handler.rb'. It is created only for version one so we can support multiple exception handlers for futher versions developed in the future.
     - exception handler module for version 1 included in the version 1 'base.rb' file.
     - Added more documentation on how we can approach error handling in our project inside github wiki/grape_framework. Thus, the whole team does not have to lose time with long documentations and can easily learn and understand the handling error approach for the project.

--- app/api/badi/v1/base.rb
+++ app/api/badi/v1/base.rb
@@ -2,6 +2,7 @@ module Badi
   module V1
     class Base < Grape::API
       version 'V1', using: :path
+      include Badi::V1::ExceptionHandler
       # TODO: Mount classes to respond to api calls
     end
   end
