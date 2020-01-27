98c749b53c5e342052f5772459b39e7f71990f7c
unknown <arnald.auli@gmail.com>
2020-01-27 11:37:16 +0100

    share actual code to team mates and get exception module from them. I need exception module to make de rspec and grape

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name { "user  name" }
+    birth_date  {'1981-10-10'}
+    gender {"Male"}
+    about { " la mare del tano quan comprava butano "}
+    created_at {'2019-10-10'}
+    updated_at {'2019-10-10'}
+  end
+end
