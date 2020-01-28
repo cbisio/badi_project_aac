5c00c486c62155db7afc2755b969ad436c6b4de7
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2020-01-28 19:50:00 +0100

    Refactoring geocoding service to raise custom error from within the object service itself. Furthermore, this includes changing the excepcion_handler.rb file to handle this specific error.

--- app/api/badi/v1/exception_handler.rb
+++ app/api/badi/v1/exception_handler.rb
@@ -3,18 +3,14 @@ module Badi
     module ExceptionHandler
       extend ActiveSupport::Concern
 
-      class GeocodingServiceError < StandardError; end
       class RoomNotFound < StandardError; end
 
       included do
-        rescue_from Badi::V1::ExceptionHandler::GeocodingServiceError do |e|
-          error!(e.message, 424)
-        end
 
         rescue_from Grape::Exceptions::ValidationErrors do |e|
           error!(e.message, 422)
         end
-        
+
         rescue_from Badi::V1::ExceptionHandler::RoomNotFound do |e|
           error!(e.message, 404)
         end
@@ -22,6 +18,11 @@ module Badi
         rescue_from ActiveRecord::RecordInvalid do |e|
           error!(e.message, 422)
         end
+
+        rescue_from GeocodingService::ThirdPartyError do |e|
+          error!(e.message, 424)
+        end
+
       end
     end
   end
