e23e41f4143d0e5b571024521760af8be79711e2
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2020-01-31 20:02:35 +0100

    Elasticsearch integration
    DONE:
     - Both elasticsearch rails and model gems added to Gemfile.
     - Added necessary dependencies and configuration to Room model so its attributes can be indexed and therefore be used to carry out elasticsearch searches.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,14 +1,37 @@
+require 'elasticsearch/model'
+
 class Room < ApplicationRecord
+  # Elastic Search
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  settings do
+    mappings dynamic: false do
+      indexes :location, type: :geo_point
+      indexes :city, type: :object do
+        indexes :name
+      end
+    end
+  end
+
+  # Customize the JSON serialization for Elasticsearch
+  def as_indexed_json(options={})
+    as_json(include: { city: { only: :name } }).merge location: { lat: latitude, lon: longitude }
+  end
+
+  # Associations
   belongs_to :user
   belongs_to :city
   has_many :photos
   has_and_belongs_to_many :extras
 
+  # Validations
   validates :name, length: { minimum: 5, maximum: 80}, presence: true
   validates :address, length: { minimum: 10, maximum: 200}, presence: true
   validates :price, :numericality => { :greater_than => 10, :less_than => 10000}, presence: true
   validates :latitude, :numericality => { :greater_than => -90, :less_than => 90}, presence: true
   validates :longitude, :numericality => { :greater_than => -180, :less_than => 180}, presence: true
   validates :currency, :inclusion => {:in => ['€', '$']}, presence: true
-
 end
+
+
