658ef6b5c3c32b272ccb8d62f4bec728d7f7f8f9
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2020-02-03 18:39:27 +0100

    Improving test coverage
    DONE:
     - Implementing tests in code areas that were not preivously tested when using TDD.
     - Test coverage is now up to 98%.

---
+++ spec/services/geocoding_service_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe GeocodingService do
+  let(:text) { "Balmes,Barcelona" }
+  let(:opts_empty) { {} }
+  let(:search_results) { [{ "name": "location1" }, {"name": "location2"}] }
+  
+  context 'when third party API (TomTom) response code is 200' do
+    it 'returns a Success object with the obtained data' do
+      geocoding_service = GeocodingService.new(text, opts_empty)
+      ok = double('Location Search results', code: 200, body: '{ "summary": {}, "results": [{ "name": "location1" }, {"name": "location2"}] }')
+      expect(geocoding_service).to receive(:request_geocoding_third_api_service).and_return(ok)
+      expect(geocoding_service).to receive(:clean_locations_response).and_return(search_results)
+      expect(ObjectService::Success).to receive(:new).with(search_results)
+      geocoding_service.call
+    end
+  end
+
+  context 'when third party API (TomTom) response code is different than 200' do
+    it 'returns an error object' do
+      geocoding_service = GeocodingService.new(text, opts_empty)
+      failure = double('Location Search results', code: 404)
+      expect(geocoding_service).to receive(:request_geocoding_third_api_service).and_return(failure)
+      expect(ObjectService::Error).to receive(:new).with(I18n.t('geocoding_service.errors.third_party_error'), 422)
+      geocoding_service.call
+    end
+  end
+end
--- spec/services/search_rooms_service_spec.rb
+++ spec/services/search_rooms_service_spec.rb
@@ -1,22 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe SearchRoomsService do
-  context 'when there is pagination' do
-    let(:search_room_service) { SearchRoomsService.new(
+  let(:top_left_point) {
     {
       top_left_lat: 41.5,
       top_left_lon: 2.0
-    },
+    }
+  }
+
+  let(:bottom_right_point) {
     {
       bottom_right_lat: 41.3,
       bottom_right_lon: 2.3
-    },
+    }
+  }
+
+  let(:opts_empty) {
+    {
+
+    }
+  }
+
+  let(:opts_with_pagination) {
     {
       city: "Barcelona",
       page: 10,
       size: 2
-    })}
+    }
+  }
 
+  let(:opts_without_pagination) {
+    {
+      city: "Barcelona"
+    }
+  }
+
+  context 'when there is pagination' do
     it 'searches rooms with correct query' do
       results = double("Elastic Search results", results: ["Room1", "Room 2"])
       expect(Room).to receive(:search).with(query:
@@ -46,24 +65,11 @@ RSpec.describe SearchRoomsService do
 
       expect(ObjectService::Success).to receive(:new)
 
-      search_room_service.call
+      SearchRoomsService.call(top_left_point, bottom_right_point, opts_with_pagination)
     end
   end
 
   context 'when there is no pagination' do
-    let(:search_room_service) { SearchRoomsService.new(
-    {
-      top_left_lat: 41.5,
-      top_left_lon: 2.0
-    },
-    {
-      bottom_right_lat: 41.3,
-      bottom_right_lon: 2.3
-    },
-    {
-      city: "Barcelona"
-    })}
-
     it 'searches rooms with correct query' do
       results = double('Elastic Search results', results: ["ROOM1", "ROOM2"])
       expect(Room).to receive(:search).with(query:
@@ -91,53 +97,56 @@ RSpec.describe SearchRoomsService do
 
       expect(ObjectService::Success).to receive(:new)
 
-      search_room_service.call
+      SearchRoomsService.call(top_left_point, bottom_right_point, opts_without_pagination)
     end
   end
 
   context 'when it finds rooms' do
-    let(:search_room_service) { SearchRoomsService.new(
-      {
-        top_left_lat: 41.5,
-        top_left_lon: 2.0
-      },
-      {
-        bottom_right_lat: 41.3,
-        bottom_right_lon: 2.3
-      },
-      {
-        city: "Barcelona",
-        page: 10,
-        size: 2
-      })}
     it 'returns a success object' do
       results = double('Elastic Search results', results: ["ROOM1", "ROOM2"])
       expect(Room).to receive(:search).and_return(results)
       expect(ObjectService::Success).to receive(:new)
-      search_room_service.call
+      SearchRoomsService.call(top_left_point, bottom_right_point, opts_with_pagination)
     end
   end
 
   context 'when it does not find rooms' do
-    let(:search_room_service) { SearchRoomsService.new(
-      {
-        top_left_lat: 41.5,
-        top_left_lon: 2.0
-      },
-      {
-        bottom_right_lat: 41.3,
-        bottom_right_lon: 2.3
-      },
-      {
-        city: "Barcelona",
-        page: 10,
-        size: 2
-      })}
     it 'returns an error object' do
       results = double('Elastic Search results', results: [])
       expect(Room).to receive(:search).and_return(results)
       expect(ObjectService::Error).to receive(:new)
-      search_room_service.call
+      SearchRoomsService.call(top_left_point, bottom_right_point, opts_with_pagination)
+    end
+  end
+
+  context 'when no city name is provided' do
+    it 'searches rooms with correct query' do
+      results = double("Elastic Search results", results: ["Room1", "Room 2"])
+      expect(Room).to receive(:search).with(query:
+      {
+        bool: {
+          must: [
+          {
+            geo_bounding_box: {
+              location: {
+                top_left: {
+                  lat: 41.5,
+                  lon: 2.0
+                },
+                bottom_right: {
+                  lat: 41.3,
+                  lon: 2.3
+                }
+              }
+            }
+          }
+          ]
+        }
+      }).and_return(results)
+
+      expect(ObjectService::Success).to receive(:new)
+
+      SearchRoomsService.call(top_left_point, bottom_right_point, opts_empty)
     end
   end
 end
