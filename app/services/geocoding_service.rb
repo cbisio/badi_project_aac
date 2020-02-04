32d4c6745e9edd919c3e45813a5610e199c58052
Albert Llebaria Fabriàs <albertllebariafabrias@MacBook-MacBook-Pro-de-Albert.local>
2020-02-04 13:19:16 +0100

    search endpoint now encodes url before requesting tomtom using http party

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -45,7 +45,7 @@ class GeocodingService < ObjectService
   end
 
   def url
-    "https://api.tomtom.com/search/2/search/#{@text}.json"
+    URI.encode("https://api.tomtom.com/search/2/search/#{@text}.json")
   end
 
   def clean_locations_response(dirty_locations)
